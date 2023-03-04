97054e0b1f22e9d18b0c26844d5c0fddc85b87f4
Suryoday Yadav <suryayadav181194@gmail.com>
2023-03-04 23:42:05 +0530

    frontend changes, making login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      Hello Client
+      <BrowserRouter>
+				<Route path="/login" exact component={Login} />
+				<Route path="/register" exact component={Register} />
+				<Route path="/dashboard" exact component={Dashboard} />
+			</BrowserRouter>  
     </div>
   );
 }
